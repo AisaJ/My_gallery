29ed6ca012fc3e824afb8c04a90e824eaf5ca5c4
Aisa <aisajemila@gmail.com>
2019-05-11 16:43:38 +0300

    Added function for displaying the images in category.

--- my_gallery/urls.py
+++ my_gallery/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-  url('^$',views.home,name='galeria'),
+  url(r'^$',views.home,name='galeria'),
+  url(r'^images(categry)',views.image,name='image')
 ]
--- my_gallery/views.py
+++ my_gallery/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return render(request,'home.html')
+
+
+def image(request,category):
+  try:
+    image = Image.objects.get(category)
+  except DoesNotExist:
+    raise Http404()
+  return render(request,'make-gallery/image.html',{'image':image})
