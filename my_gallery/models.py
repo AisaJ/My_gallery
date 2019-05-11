a1d203e8156788754faf7f5421f8d911abf8f04a
Aisa <aisajemila@gmail.com>
2019-05-11 18:02:59 +0300

    created an admin side.

--- my_gallery/models.py
+++ my_gallery/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class Image(models.Model):
-  image = models.ImageField(upload_to='image/' default='grad.jpeg')
-  name = models.CharField(max_length=40)
-  description = models.TextField()
-  location = models.ForeignKey(Location)
-  category = models.ForeignKey(Category)
-
-  def __str__(self):
-    return self.name
-
+# Create your models here
 class Location(models.Model):
   name = models.CharField(max_length=40)
 
   def __str__(self):
     return self.name
+
 class Category(models.Model):
   name = models.CharField(max_length=40)
+
+  def __str__(self):
+    return self.name
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='image/',default='grad.jpeg')
+  name = models.CharField(max_length=40)
+  description = models.TextField()
+  location = models.ForeignKey(Location)
+  category = models.ForeignKey(Category)
 
   def __str__(self):
     return self.name
