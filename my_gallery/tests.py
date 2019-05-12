956387c703943220444591f82eadb75d8dd280df
Aisa <aisajemila@gmail.com>
2019-05-12 20:25:43 +0300

    create a test case for saving image object.

--- my_gallery/tests.py
+++ my_gallery/tests.py
@@ -2,12 +2,21 @@ from django.test import TestCase
 from .models import Image,Category,Location
 
 # Create your tests here.
-class ImageTestClass(TestCase):
+class GalleryTestClass(TestCase):
   #Set up method
   def setUp(self):
     self.kids_weekend = Image(image ='sneakers.jpeg',name='Sneakers',description='Bright colored and light')
+    self.mombasa = Location(name='Mombasa')
+    self.sports = Category(name='Sports')
 
   #Testing instance
   def test_instance(self):
     self.assertTrue(isinstance(self.kids_weekend,Image))
+    self.assertTrue(isinstance(self.mombasa.Location))
+    self.assertTrue(isinstance(self.sports.Category))
 
+  #Testing Save Method
+  def test_save_method(self):
+    self.kids_weekend.save_image()
+    images = Image.objects.all()
+    self.assertTrue(len(images > 0))
