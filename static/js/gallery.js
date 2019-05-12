1740f333f342f2f1970e7e61af760b2afdd74f69
Aisa <aisajemila@gmail.com>
2019-05-12 12:14:26 +0300

    Changed the fancybox linking files to cdn link.

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,1 +1,31 @@
+$(document).ready(function(){
+  $('.fancybox').fancybox({
+      beforeShow: function() {
+        this.title = $(this.element).data("caption");
+      }
+  });
 
+  $('[data-fancybox]').fancybox({
+    protect: true,
+    closeExisting: false,
+    keyboard: true,
+    preventCaptionOverlap: true,
+    arrows: true,
+    loop:true,
+    buttons: [
+      "zoom",
+      //"share",
+      "slideShow",
+      "fullScreen",
+      "download",
+      "thumbs",
+      "close"
+    ],
+    animationEffect: "fade",
+    selector : '[data-fancybox="gallery"]',
+    loop     : true,
+    caption : function(instance, item) {
+      return this.dataset.caption || '';
+    }
+  });
+});
